7627d373495ddef497f1ab3b1ac8b85a9a2c512c
sylvioneto <sylvio.pedroza@gmail.com>
2020-06-27 13:53:19 -0600

    add examples

---
+++ examples/startup.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+curl -sSO https://dl.google.com/cloudagents/install-monitoring-agent.sh
+sudo bash install-monitoring-agent.sh
+
+curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
+sudo bash install-logging-agent.sh
+sudo apt update -y
+sudo apt install -y nginx
