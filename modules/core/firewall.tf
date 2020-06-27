aab5f3221cd642e36dd43174ff1ad35d24f22853
sylvioneto <sylvio.pedroza@gmail.com>
2020-06-27 14:19:51 -0600

    update firewall with comment

--- modules/core/firewall.tf
+++ modules/core/firewall.tf
@@ -26,7 +26,10 @@ resource "google_compute_firewall" "allow_external_ssh" {
   description   = "Allow SSH to the instances from external source"
   network       = google_compute_network.vpc.self_link
   direction     = "INGRESS"
-  source_ranges = ["35.235.240.0/20", var.ssh_cidr]
+  source_ranges = [
+    "35.235.240.0/20", // Google IAP https://cloud.google.com/iap/docs/using-tcp-forwarding
+    var.ssh_cidr
+  ]
   target_tags   = ["allow-ssh"]
 
   allow {
