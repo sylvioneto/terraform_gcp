1ea2eb82525ffd64cadca14918a91792ac06eb71
sylvioneto <sylvio.pedroza@gmail.com>
2020-11-07 10:51:07 -0600

    gke - cluster module

---
+++ modules/gke/gke.tf
@@ -0,0 +1,51 @@
+resource "google_container_cluster" "gke" {
+  name     = var.name
+  location = var.region
+
+  default_max_pods_per_node = var.default_max_pods_per_node
+  remove_default_node_pool  = var.remove_default_node_pool
+  initial_node_count        = 1
+
+  master_auth {
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+
+  release_channel {
+    channel = var.release_channel
+  }
+
+  // Network settings
+  network    = var.vpc
+  subnetwork = google_compute_subnetwork.gke_subnet.self_link
+  private_cluster_config {
+    enable_private_nodes    = true
+    enable_private_endpoint = true
+    master_ipv4_cidr_block  = var.ip_allocation_ranges["master"]
+  }
+  ip_allocation_policy {
+    cluster_secondary_range_name  = "pods"
+    services_secondary_range_name = "services"
+  }
+
+  // Nodes config
+  node_config {
+    preemptible  = true
+    machine_type = var.machine_type
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+    labels = local.labels
+  }
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+}
--- modules/gke/variables.tf
+++ modules/gke/variables.tf
@@ -22,13 +22,13 @@ variable "vpc" {
 
 variable "ip_cidr_range" {
   type        = string
-  description = "Subnet ip_cidr_rang"
+  description = "CIDR where the nodes will be placed."
   default     = "10.1.0.0/22"
 }
 
 variable "ip_allocation_ranges" {
   type        = map
-  description = "CIDR map for cluster, pods, and services."
+  description = "CIDR map for master, pods, and services."
   default = {
     pods     = "10.1.4.0/22",
     services = "10.1.8.0/24",
@@ -39,4 +39,28 @@ variable "ip_allocation_ranges" {
 variable "labels" {
   description = "Additional labels"
   default     = {}
+}
+
+variable "machine_type" {
+  type        = string
+  description = "GCP machine type"
+  default     = "g1-small"
+}
+
+variable "remove_default_node_pool" {
+  type        = bool
+  description = "Remove the default node pool"
+  default     = true
+}
+
+variable "release_channel" {
+  type        = string
+  description = "GKE updates release channel."
+  default     = "STABLE"
+}
+
+variable "default_max_pods_per_node" {
+  type        = number
+  description = "default max pods per node"
+  default     = 32
 }
