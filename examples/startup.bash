1779c309f4316b91aab4b18ba049439b05346035
sylvio pedroza <sylvio.pedroza@gmail.com>
2021-01-08 14:07:27 -0600

    move examples to the readmes

--- examples/startup.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-curl -sSO https://dl.google.com/cloudagents/install-monitoring-agent.sh
-sudo bash install-monitoring-agent.sh
-
-curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
-sudo bash install-logging-agent.sh
-sudo apt update -y
-sudo apt install -y nginx
