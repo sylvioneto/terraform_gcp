d2d2440266c899a61425d8fffb9802fe4e5681f6
sylvio.pedroza <sylvio.pedroza@gmail.com>
2021-07-13 22:53:35 +0000

    test

--- sole-tenant/vms.tf
+++ sole-tenant/vms.tf
@@ -10,8 +10,7 @@ resource "google_compute_instance" "app1" {
   }
 
   network_interface {
-    network    = google_compute_network.vpc.id
-    subnetwork = google_compute_subnetwork.subnet.id
+    subnetwork = google_compute_subnetwork.subnet.self_link
 
     access_config {
       // Ephemeral IP
@@ -39,8 +38,7 @@ resource "google_compute_instance" "app2" {
   }
 
   network_interface {
-    network    = google_compute_network.vpc.id
-    subnetwork = google_compute_subnetwork.subnet.id
+    subnetwork = google_compute_subnetwork.subnet.self_link
 
     access_config {
       // Ephemeral IP
@@ -63,17 +61,36 @@ resource "google_compute_instance" "db1" {
 
   boot_disk {
     initialize_params {
-      image = "windows-sql-cloud/sql-2016-standard-windows-2016-dc-v20210608"
+      image = "windows-cloud/windows-server-2016-dc-v20210608"
     }
   }
 
   network_interface {
-    network    = google_compute_network.vpc.id
-    subnetwork = google_compute_subnetwork.subnet.id
+    subnetwork = google_compute_subnetwork.subnet.self_link
+  }
 
-    access_config {
-      // Ephemeral IP
+  scheduling {
+    node_affinities {
+      key      = "compute.googleapis.com/node-group-name"
+      operator = "IN"
+      values   = [google_compute_node_group.soletenant_grp.name]
     }
+  }
+}
+
+resource "google_compute_instance" "linux" {
+  name         = "srv-app-003"
+  machine_type = "n1-standard-2"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet.self_link
   }
 
   scheduling {
