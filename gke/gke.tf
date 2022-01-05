bf32eda27bfcc41b39c1dc79db283c9a9eeb58ed
sylvio pedroza <sylvio.pedroza@gmail.com>
2022-01-05 18:13:55 -0300

    add project_id

--- gke/gke.tf
+++ gke/gke.tf
@@ -3,6 +3,7 @@
 module "gke" {
   source     = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
   version    = "~> 17.0.0"
+  project_id = var.project_id
   region     = local.region
   name       = local.cluster_name
 
