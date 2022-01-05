45b79c186ea151b02425e0593fdc0885e228bf42
sylvio pedroza <sylvio.pedroza@gmail.com>
2022-01-05 18:59:36 -0300

    subnet

--- sole-tenant/network.tf
+++ sole-tenant/network.tf
@@ -5,7 +5,7 @@ resource "google_compute_network" "vpc" {
 }
 
 resource "google_compute_subnetwork" "subnet" {
-  name                     = "subnet-win"
+  name                     = "win-subnet"
   description              = "Subnet to host Windows Server VMs"
   ip_cidr_range            = "10.1.0.0/16"
   region                   = "us-central1"
