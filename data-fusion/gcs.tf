a1b9cd6e6cd04b1d105f8f08e138a7ead9d742c5
sylvio pedroza <sylvio.pedroza@gmail.com>
2022-01-14 15:43:07 -0300

    minor

--- data-fusion/gcs.tf
+++ data-fusion/gcs.tf
@@ -1,20 +1,23 @@
 resource "google_storage_bucket" "raw" {
-  name          = "${var.project_id}-data-raw"
-  location      = "us-east1"
-  force_destroy = true
-  labels        = local.labels
+  name                        = "${var.project_id}-data-raw"
+  location                    = "us-east1"
+  force_destroy               = true
+  uniform_bucket_level_access = true
+  labels                      = local.labels
 }
 
 resource "google_storage_bucket" "data_lake" {
-  name          = "${var.project_id}-data-raw"
-  location      = "us-east1"
-  force_destroy = true
-  labels        = local.labels
+  name                        = "${var.project_id}-data-raw"
+  location                    = "us-east1"
+  force_destroy               = true
+  uniform_bucket_level_access = true
+  labels                      = local.labels
 }
 
 resource "google_storage_bucket" "data_warehouse" {
-  name          = "${var.project_id}-data-warehouse"
-  location      = "us-east1"
-  force_destroy = true
-  labels        = local.labels
+  name                        = "${var.project_id}-data-warehouse"
+  location                    = "us-east1"
+  force_destroy               = true
+  uniform_bucket_level_access = true
+  labels                      = local.labels
 }
