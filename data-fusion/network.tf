c7082f6795131940c8b1128024ca91eef0831c29
sylvio pedroza <sylvio.pedroza@gmail.com>
2022-01-14 18:19:11 -0300

    test

--- data-fusion/network.tf
+++ data-fusion/network.tf
@@ -18,30 +18,6 @@ module "vpc" {
   ]
 }
 
-# NAT and Router
-resource "google_compute_router" "nat_router" {
-  name    = "${module.vpc.network_name}-nat-router"
-  network = module.vpc.network_self_link
-
-  bgp {
-    asn = 64514
-  }
-}
-
-resource "google_compute_router_nat" "nat_gateway" {
-  name                               = "${module.vpc.network_name}-nat-gw"
-  router                             = google_compute_router.nat_router.name
-  region                             = google_compute_router.nat_router.region
-  nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-
-  log_config {
-    enable = true
-    filter = "ERRORS_ONLY"
-  }
-}
-
-
 # Allow internal trrafic
 resource "google_compute_firewall" "allow-vm-to-vm" {
   name    = "allow-vm-to-vm"
