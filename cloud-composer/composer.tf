4e0b727ebfc28c84c6a1b3b1937252a9545af666
sylvio pedroza <sylvio.pedroza@gmail.com>
2022-01-17 15:07:13 -0300

    add subnets_ids

--- cloud-composer/composer.tf
+++ cloud-composer/composer.tf
@@ -2,4 +2,13 @@ resource "google_composer_environment" "dev" {
   name   = "composer-dev"
   region = var.region
   labels = local.labels
+
+  config {
+    node_count = 4
+
+    node_config {
+      network    = module.vpc.network_self_link
+      subnetwork = module.vpc.subnets_ids[0]
+    }
+  }
 }
