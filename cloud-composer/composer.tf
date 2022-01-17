28e90ef88412edc49756dd84682434a20566d0e7
sylvio pedroza <sylvio.pedroza@gmail.com>
2022-01-17 15:10:49 -0300

    test

--- cloud-composer/composer.tf
+++ cloud-composer/composer.tf
@@ -4,11 +4,22 @@ resource "google_composer_environment" "airflow_dev" {
   labels = local.labels
 
   config {
-    node_count = 4
-
     node_config {
+      zone         = "us-central1-a"
+      machine_type = "n1-standard-1"
+
       network    = module.vpc.network_self_link
       subnetwork = module.vpc.subnets_ids[0]
+
+      service_account = google_service_account.test.name
+    }
+
+    database_config {
+      machine_type = "db-n1-standard-2"
+    }
+
+    web_server_config {
+      machine_type = "composer-n1-webserver-2"
     }
   }
 }
