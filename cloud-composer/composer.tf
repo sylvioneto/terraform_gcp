0967bd233433c89ff4d4b4fec85e669b23de1cff
sylvio pedroza <sylvio.pedroza@gmail.com>
2022-01-17 14:54:36 -0300

    test

--- cloud-composer/composer.tf
+++ cloud-composer/composer.tf
@@ -2,12 +2,4 @@ resource "google_composer_environment" "dev" {
   name   = "composer-dev"
   region = var.region
   labels = local.labels
-
-  config {
-    node_count = 4
-
-    node_config {
-      network    = module.vpc.network_self_link
-    }
-  }
 }
