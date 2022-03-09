1384ad8e555677b9493df8b1b2b1c59ae4e93f80
sylvio pedroza <sylvio.pedroza@gmail.com>
2022-03-09 11:58:40 -0300

    shell

--- shared-vpc/run_terraform.sh
+++ shared-vpc/run_terraform.sh
@@ -1,5 +1,8 @@
 #!bin/sh
 
+export TF_VAR_org_id=$ORG_ID
+export TF_VAR_billing_account_id=$BILLING_ACCOUNT_ID
+
 terraform init -backend-config="bucket=$GOOGLE_CLOUD_PROJECT-tf-state" -backend-config="prefix=terraform_gcp/shared_vpc"
 
 terraform plan -out shared_vpc.tfplan
