dfd11f7ba3f1b2a84798c23e687df6645baf2765
sylvio pedroza <sylvio.pedroza@gmail.com>
2022-03-09 11:54:03 -0300

    minor

--- shared-vpc/run_terraform.sh
+++ shared-vpc/run_terraform.sh
@@ -2,6 +2,6 @@
 
 terraform init -backend-config="bucket=$GOOGLE_CLOUD_PROJECT-tf-state" -backend-config="prefix=terraform_gcp/shared_vpc"
 
-terraform plan
+terraform plan -out shared_vpc.tfplan
 
-terraform apply -auto-approve
+terraform apply shared_vpc.tfplan
