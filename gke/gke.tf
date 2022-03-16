0c6571928ff65530cc32484c42512a8525253162
sylvio pedroza <sylvio.pedroza@gmail.com>
2022-03-16 18:07:17 -0300

    add kubernetes provider

--- gke/gke.tf
+++ gke/gke.tf
@@ -1,4 +1,11 @@
 # Ref https://github.com/terraform-google-modules/terraform-google-kubernetes-engine/tree/master/modules/private-cluster
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
 
 module "gke" {
   source     = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
