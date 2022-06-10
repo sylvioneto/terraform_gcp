f62291600eab05ed0b5fcbe52c7ed182d66961fd
sylvio pedroza <sylvio.pedroza@gmail.com>
2022-06-10 16:11:03 -0300

    missing project id

--- cloud-armor/terraform/service_account.tf
+++ cloud-armor/terraform/service_account.tf
@@ -4,11 +4,13 @@ resource "google_service_account" "service_account" {
 }
 
 resource "google_project_iam_member" "log_writer" {
-  role   = "roles/logging.logWriter"
-  member = "serviceAccount:${google_service_account.service_account.email}"
+  project = var.project_id
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
 }
 
 resource "google_project_iam_member" "monitoring_writer" {
-  role   = "roles/monitoring.metricWriter"
-  member = "serviceAccount:${google_service_account.service_account.email}"
+  project = var.project_id
+  role    = "roles/monitoring.metricWriter"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
 }
