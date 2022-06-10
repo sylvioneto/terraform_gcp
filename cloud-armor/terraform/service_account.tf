5cb4764d607f67598324aba5d8e7d8e6aa07c2d8
sylvio pedroza <sylvio.pedroza@gmail.com>
2022-06-10 16:09:25 -0300

    least privilege to sa

--- cloud-armor/terraform/service_account.tf
+++ cloud-armor/terraform/service_account.tf
@@ -3,14 +3,12 @@ resource "google_service_account" "service_account" {
   display_name = "Service Account for instances of ${local.application_name}"
 }
 
-resource "google_service_account_iam_member" "log_writer" {
-  service_account_id = google_service_account.service_account.name
-  role               = "roles/logging.logWriter"
-  member             = "serviceAccount:${google_service_account.service_account.email}"
+resource "google_project_iam_member" "log_writer" {
+  role   = "roles/logging.logWriter"
+  member = "serviceAccount:${google_service_account.service_account.email}"
 }
 
-resource "google_service_account_iam_member" "monitoring_writer" {
-  service_account_id = google_service_account.service_account.name
-  role               = "roles/monitoring.metricWriter"
-  member             = "serviceAccount:${google_service_account.service_account.email}"
+resource "google_project_iam_member" "monitoring_writer" {
+  role   = "roles/monitoring.metricWriter"
+  member = "serviceAccount:${google_service_account.service_account.email}"
 }
