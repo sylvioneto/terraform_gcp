4a82e9ffdb6a1e6915ec1447c1fa4d389a3f8179
sylvio pedroza <sylvio.pedroza@gmail.com>
2022-06-30 15:24:47 -0300

    skip leading lines

--- cloud-composer/dags/mssql_to_bq.py
+++ cloud-composer/dags/mssql_to_bq.py
@@ -19,10 +19,14 @@ from datetime import datetime
 
 from airflow import models
 from airflow.providers.google.cloud.transfers.mssql_to_gcs import MSSQLToGCSOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 
+
+BUCKET='mssql-data'
+FILENAME='data/country/export.csv'
 
 with models.DAG(
-    'mssql_to_gcs',
+    'mssql_to_bq',
     schedule_interval='@once',
     start_date=datetime(2021, 12, 1),
     catchup=False,
@@ -32,7 +36,23 @@ with models.DAG(
         task_id='mssql_to_gcs',
         mssql_conn_id='airflow_mssql',
         sql=r"""SELECT * FROM Country;""",
-        bucket='mssql-data',
-        filename='data/country/export',
+        bucket=BUCKET,
+        filename=FILENAME,
         export_format='csv',
     )
+
+    load_csv = GCSToBigQueryOperator(
+        task_id='gcs_to_bigquery',
+        bucket=BUCKET,
+        source_objects=[FILENAME],
+        destination_project_dataset_table=f"raw.country",
+        schema_fields=[
+            {'name': 'country_id', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+            {'name': 'country', 'type': 'STRING', 'mode': 'NULLABLE'},
+            {'name': 'continent', 'type': 'STRING', 'mode': 'NULLABLE'},
+        ],
+        skip_leading_rows=1,
+        write_disposition='WRITE_TRUNCATE',
+    )
+
+    upload >> load_csv
