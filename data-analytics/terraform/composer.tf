09e90a2942eb188c769105f9bf7643bee9b98272
sylvio pedroza <sylvio.pedroza@gmail.com>
2022-06-30 08:26:36 -0300

    minor

--- data-analytics/terraform/composer.tf
+++ data-analytics/terraform/composer.tf
@@ -2,14 +2,15 @@ module "composer" {
   source  = "terraform-google-modules/composer/google//modules/create_environment_v2"
   version = "~> 3.1"
 
-  project_id              = var.project_id
-  composer_env_name       = local.composer_env_name
-  region                  = var.region
-  network                 = module.vpc.network_name
-  subnetwork              = local.composer_env_name
-  enable_private_endpoint = true
-  labels                  = local.resource_labels
-  image_version           = "composer-2-airflow-2"
+  project_id               = var.project_id
+  composer_env_name        = local.composer_env_name
+  region                   = var.region
+  network                  = module.vpc.network_name
+  subnetwork               = local.composer_env_name
+  enable_private_endpoint  = true
+  labels                   = local.resource_labels
+  image_version            = "composer-2-airflow-2"
+  composer_service_account = google_service_account.service_account.email
 
   depends_on = [
     module.vpc
--- data-analytics/terraform/iam.tf
+++ data-analytics/terraform/iam.tf
@@ -3,6 +3,19 @@ resource "google_service_account" "service_account" {
   display_name = "Service Account for instances of ${local.composer_env_name}"
 }
 
+# required permissions for env provisioning
+resource "google_project_iam_member" "composer_worker" {
+  project = var.project_id
+  role    = "roles/composer.worker"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+resource "google_project_iam_member" "composer_worker" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
 resource "google_project_iam_member" "log_writer" {
   project = var.project_id
   role    = "roles/logging.logWriter"
@@ -14,6 +27,8 @@ resource "google_project_iam_member" "monitoring_writer" {
   role    = "roles/monitoring.metricWriter"
   member  = "serviceAccount:${google_service_account.service_account.email}"
 }
+
+# required for managing data
 
 resource "google_project_iam_member" "bq_admin" {
   project = var.project_id
