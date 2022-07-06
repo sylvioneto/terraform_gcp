bcbe5f1d185b8bdcf5a967ed2127ab892b216882
sylvio pedroza <sylvio.pedroza@gmail.com>
2022-07-06 16:08:57 -0300

    notes

--- cloud-composer/dags/mssql_to_bq.py
+++ cloud-composer/dags/mssql_to_bq.py
@@ -1,19 +1,8 @@
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
+# This dag demonstrates how to take the data from a SQL SERVER instance to BigQuery
+# In order to run it, please prepare your database first:
+# 1) setup the connection airflow_mssql in your Cloud Composer
+# 2) load AdventureWorks2019 sample data to your database
+
 import os
 from datetime import datetime
 
