fcadc5b372fae74234fb7c8b06fed7ea944a2f14
sylvio pedroza <sylvio.pedroza@gmail.com>
2022-08-16 10:18:43 -0300

    restore backup with cloud build

--- cloud-composer/terraform/gcs.tf
+++ cloud-composer/terraform/gcs.tf
@@ -1,4 +1,8 @@
 resource "google_storage_bucket" "sql_backup" {
-  name = "${var.project_id}-sql-backup"
-  location = var.region
+  name                        = "${var.project_id}-sql-backup"
+  location                    = var.region
+  uniform_bucket_level_access = true
+  lifecycle {
+    prevent_destroy = false
+  }
 }
