0d9737767cd93baac5df0699e700770fc0729a49
sylvio pedroza <sylvio.pedroza@gmail.com>
2022-08-16 13:10:11 -0300

    gcs composer

--- cloud-composer/terraform/gcs.tf
+++ cloud-composer/terraform/gcs.tf
@@ -2,16 +2,12 @@ resource "google_storage_bucket" "sql_backup" {
   name                        = "${var.project_id}-sql-backup"
   location                    = var.region
   uniform_bucket_level_access = true
-  lifecycle {
-    prevent_destroy = false
-  }
+  force_destroy               = true
 }
 
 resource "google_storage_bucket" "data_lake" {
   name                        = "${var.project_id}-data-lake"
   location                    = var.region
   uniform_bucket_level_access = true
-  lifecycle {
-    prevent_destroy = false
-  }
+  force_destroy               = true
 }
